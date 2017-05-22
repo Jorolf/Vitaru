030d3b4b1ed8c752ccf5b917119ef06bae082a78
Jorolf <jorolf@gmx.de>
2017-05-22 16:52:06 +0200

    add vitaru.Desktop an osu.Game wrapper to load vitaru

---
+++ vitaru.Desktop/Program.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) 2007-2017 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using System;
+using osu.Desktop;
+
+namespace vitaru.Desktop
+{
+    public static class Program
+    {
+        [STAThread]
+        public static int Main(string[] args)
+        {
+            return osu.Desktop.Program.Main(args);
+        }
+    }
+}
